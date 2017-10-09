ec9a98d8200765d8666e975c5c0746a152a8a3a8
Ma5ee2 <hussainmasee@gmail.com>
2017-10-09 09:30:54 +0100

    added some code to access each mole hole

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('Wasssssup');
+$(() =>{
+
+//Declare variable to access all the holes.
+
+const $li = $('.holes');
+
+
+//Declare the click event.
+$li.on('click', hole);
+
+function hole(e) {
+  const $clickedMole = $(e.target);
+  console.log($clickedMole);
+}
+
+
+
+
+
+
+});
