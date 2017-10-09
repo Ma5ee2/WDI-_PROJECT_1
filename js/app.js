13931f296d4e88e284ab127624be940fa74f5c0d
Ma5ee2 <hussainmasee@gmail.com>
2017-10-09 11:20:26 +0100

    added the random hole event

--- js/app.js
+++ js/app.js
@@ -1,21 +1,50 @@
-$(() =>{
+// $(() => {
+//   //Declare variables.
+//   const $holes = $('.hole');
+//   // let $score   = $('.score');
+//   // let $timer   = $('.timer');
+//   // const $start = $('.start');
+//
+//   // Declare the click event.
+//   // $holes.on('click', hole);
+//   //
+//   //
+//   // function hole(e) {
+//   //   Remembers last hole clicked.
+//   //   const $clickedMole = $(e.target);
+//   //
+//   // }
+//
+//   // Random mole in hole function.
+//
+//   function randomHole($holes) {
+//     const randomMole = Math.floor(Math.random() * $holes);
+//     const hole = $holes[randomHole];
+//     console.log(hole);
+//   }
+// });
 
-//Declare variable to access all the holes.
+// 1) pickRandomHole
+// 2) addMoleToHole
+// 3) addClickToHoleWhereMoleIs
+// 3) removeMoleFromHole
+// 4) removeClickFromHoleWhereMoleWas
 
-const $li = $('.holes');
+$(setup);
 
+let $holes = null;
+console.log($holes);
 
-//Declare the click event.
-$li.on('click', hole);
-
-function hole(e) {
-  const $clickedMole = $(e.target);
-  console.log($clickedMole);
+function setup() {
+  $holes = $('.hole');
+  pickRandomHole();
 }
 
+function pickRandomHole() {
+const hole = $holes[Math.floor(Math.random()*$holes.length)];
+addMoleToHole();
+}
 
+function addMoleToHole() {
 
-
-
-
-});
+}
