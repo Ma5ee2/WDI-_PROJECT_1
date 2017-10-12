81d3d0e9e8d568a200a3abe795c87c425bc34fe5
Ma5ee2 <hussainmasee@gmail.com>
2017-10-12 15:25:36 +0100

    added sound and animation

--- js/app.js
+++ js/app.js
@@ -8,18 +8,22 @@ function setup() {
   $timer = $('.timer');
   $holes = $('.hole');
   $timeBoard = $('.time-board');
-  $startButton = ('.start');
+  $scoreBoard = $('.score-board');
+  $startButton = $('.start');
   $($startButton).on('click', startGame);
   $($holes).on('click', checkForMole);
+  // $(window).on('load', audio);
 }
 
 function checkForMole(e) {
-  if ($(this).hasClass('mole')) {
-    scoreValue++;
+  if ($(this).hasClass('mole dead-mole')) {
+    scoreValue += 5;
     $score.html(scoreValue);
+    const smashAudio = new Audio('sounds/smash.wav').play();
   } else {
-    scoreValue--;
+    scoreValue -= 8;
     $score.html(scoreValue);
+    const missAudio = new Audio('sounds/miss.wav').play();
   }
 }
 
@@ -31,13 +35,16 @@ function pickRandomHole() {
 
 function addMoleToHole(hole) {
   const mole = $(hole).addClass('mole');
-  timeOut(mole);
+  //added deadMole-----------------------------
+  const hitMole = $(hole).addClass('dead-mole');
+  timeOut(mole, hitMole);
 }
 
-function timeOut(mole) {
+function timeOut(mole, hitMole) {
   time();
   setTimeout(function() {
     $(mole).removeClass('mole');
+    $(hitMole).removeClass('dead-mole');
     if (timerLimit > 0) {
       pickRandomHole();
     } else {
@@ -52,6 +59,11 @@ function time() {
   const timer = $timer.html(timerLimit);
 }
 
+function audio() {
+  const backgroundAudio = new Audio('sounds/background.wav');
+  backgroundAudio.play();
+}
+
 function startGame() {
   $('.start').hide();
   reset();
@@ -61,7 +73,9 @@ function startGame() {
 function reset() {
   scoreValue = 0;
   timerLimit = 11;
+  $holes = $('.hole');
   $timeBoard.html('Time: <span class="timer">15</span></div>');
   $timer = $('.timer');
   $score = $('.score');
+  $timeBoard = $('.time-board');
 }
